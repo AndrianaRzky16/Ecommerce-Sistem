daaed60ca23486225c20c2f1bb76ea9ab12741d3
AndrianaRzky16 <andrianarizki42@gmail.com>
2023-09-07 14:37:36 +0700

    first

--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,8 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\OrderController;
+
 
 /*
 |--------------------------------------------------------------------------
@@ -18,3 +20,4 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
 Route::apiResource('transactions', 'TransactionController');
+Route::post('/api/orders', [OrderController::class, 'store']);
